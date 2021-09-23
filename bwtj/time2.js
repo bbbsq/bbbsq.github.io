083ea512bdd49b814850a78bbcd0e3ae60e13185
NIUNIU <74705315+bbbsq@users.noreply.github.com>
2021-09-23 16:26:29 +0800

    错错错
    擦擦擦

---
+++ bwtj/time2.js
@@ -0,0 +1,47 @@
+    // 获取当前时间
+function current() {
+    var d = new Date(),
+        str = '';
+    str += d.getFullYear() + '-'; //获取当前年份 
+    str += d.getMonth() + 1 + '-'; //获取当前月份（0——11） 
+    str += d.getDate();
+    return str;
+}
+
+
+//计算天数差的函数
+function DateDiff(sDate1, sDate2) { //sDate1和sDate2是2006-12-18格式
+    var aDate, oDate1, oDate2, iDays
+    aDate = sDate1.split("-")
+    oDate1 = new Date(aDate[0], aDate[1] - 1, aDate[2]); //调用Date的构造函数
+    aDate = sDate2.split("-");
+    oDate2 = new Date(aDate[0], aDate[1] - 1, aDate[2]);
+    iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24) //把相差的毫秒数转换为天数
+    return iDays
+}
+
+// 页面加载完成执行
+
+window.onload = function (a, b, c) {
+    var swyz = "2021-8-20"
+    var cmb = "2021-8-1"
+    var mgxq = "2021-7-7"
+    var zsdl = "2021-6-22"
+    var mym = "2021-6-22"
+    var jmkj = "2021-5-9"
+    var ep = "2021-8-26"
+    var enft = "2021-8-29"
+    var cto = "2021-8-30"
+    var star = "2021-9-3"
+    s = current();// 获取当前时间
+    document.getElementById("swyz").innerHTML = DateDiff(swyz, s);//计算天数差的函数
+    document.getElementById("cmb").innerHTML = DateDiff(cmb, s);
+    document.getElementById("mgxq").innerHTML = DateDiff(mgxq, s);
+    document.getElementById("zsdl").innerHTML = DateDiff(zsdl, s);
+    document.getElementById("mym").innerHTML = DateDiff(mym, s);
+    document.getElementById("jmkj").innerHTML = DateDiff(jmkj, s);
+    document.getElementById("ep").innerHTML = DateDiff(ep, s);
+    document.getElementById("enft").innerHTML = DateDiff(enft, s);
+    document.getElementById("cto").innerHTML = DateDiff(cto, s);
+    document.getElementById("star").innerHTML = DateDiff(star, s);
+}
---
+++ js/zc.js
@@ -0,0 +1,243 @@
+Bmob.initialize("3c1d9bdb4698443b", "10086", "12af5658d30f0016a69d61e1468f4c69");
+Bmob.debug(true)
+const query = Bmob.Query('_User'); //获取表
+
+let user = Bmob.User.current() //获取缓存
+var obj = user.objectId,
+    mz = user.username,
+    egg = user.egg,
+    sjh = user.phone,
+    sj1 = user.sj,
+    lxts = user.lxts,
+    csj = user.createdAt.substr(0, 10),
+    zt = user.zt;
+var ts = DateDiff(csj, cur()),
+    ts2 = DateDiff(sj1, cur());
+
+var time = new Date().getHours(); //获取时间
+var i = 0,
+    d = 0;
+
+function xx() {
+    if (ts2 > 1) { //判断连续签到
+        query.get(obj).then(res => {
+            console.log(res)
+            res.set('lxts', 0)
+            res.save()
+        });
+        lxts = 0;
+    }
+    if (zt != 'max') { //判断小鸡是否成年
+        if (lxts >= 2 && lxts < 9) {
+            query.get(obj).then(res => {
+                console.log(res)
+                res.set('zt', 'min')
+                res.save()
+                document.getElementById("jmm").backgroundImage = "url('./images/xjj.gif')";
+            });
+        } else if (lxts >= 9) {
+            query.get(obj).then(res => {
+                console.log(res)
+                res.set('zt', 'max')
+                res.save()
+                document.getElementById("jmm").backgroundImage = "url('./images/jmm.png')";
+            });
+        }
+        Bmob.User.updateStorage(obj) //更新缓存
+    }
+
+    if (lxts <= 10 && zt == 'max') { //判断连续<10天数后
+        if (ts % 2 == 1) {
+            d = 1;
+        }
+    } else if (lxts >= 10 && lxts <= 29 && zt == 'max') { //判断连续10到30的天数
+        d = 1;
+    } else if (lxts >= 30 && zt == 'max') { //判断连续30以上的天数
+        d = 2;
+    }
+}
+xx();
+
+function qd() {
+    if (i == 0) { //判断是否已经签到
+        if (ts2 != 0) { //判断当天是否第一次签到
+            if (ts2 == 1) { //判断是否连续签到
+                query.get(obj).then(res => {
+                    console.log(res)
+                    res.set('lxts', lxts + 1)
+                    res.save()
+                    document.getElementById('ddr').innerHTML = lxts + 1;
+                    i++;
+                }).catch(err => {
+                    alert('网络错误,刷新请重试！')
+                });
+            }
+            if (d == 1) {
+                query.get(obj).then(res => {
+                    console.log(res)
+                    res.set('egg', egg + 1)
+                    res.save()
+                    document.getElementById('d1').style.animationName = 'd1';
+                    setTimeout("document.getElementById('ds').innerHTML = egg + 1;", "2000");
+                    document.getElementById('ww').innerHTML = '已收取';
+                    i++;
+                });
+            } else if (d == 2) {
+                query.get(obj).then(res => {
+                    console.log(res)
+                    res.set('egg', egg + 2)
+                    res.save()
+                    document.getElementById('d1').style.animationName = 'd1';
+                    document.getElementById('d2').style.animationName = 'd2';
+                    setTimeout("document.getElementById('ds').innerHTML = egg + 1;", "2000");
+                    setTimeout("document.getElementById('ds').innerHTML = egg + 2;", "4000");
+                    document.getElementById('ww').innerHTML = '已收取';
+                    i++;
+                });
+            }
+            query.get(obj).then(res => { //设置今天签到日期
+                console.log(res)
+                res.set('sj', cur())
+                res.save()
+            });
+            Bmob.User.updateStorage(obj) //更新数据
+            if (zt == "max") {
+                document.getElementById('ww').innerHTML = '已收取';
+            } else if (zt == "min") {
+                document.getElementById('ww').innerHTML = '成长中';
+            } else if (zt != "min" && zt != "max") {
+                document.getElementById('ww').innerHTML = '孵化中';
+            }
+        } else {
+            alert("还没有下蛋哦！")
+        }
+    } else {
+        alert("还没有下蛋哦！")
+    }
+}
+
+function cur() {
+    var d = new Date(),
+        str = '';
+    str += d.getFullYear() + '-'; //获取当前年份 
+    str += d.getMonth() + 1 + '-'; //获取当前月份（0——11） 
+    str += d.getDate();
+    return str;
+}
+
+function DateDiff(sDate1, sDate2) { //sDate1和sDate2是2006-12-18格式
+    var aDate, oDate1, oDate2, iDays
+    aDate = sDate1.split("-")
+    oDate1 = new Date(aDate[0], aDate[1] - 1, aDate[2]); //调用Date的构造函数
+    aDate = sDate2.split("-");
+    oDate2 = new Date(aDate[0], aDate[1] - 1, aDate[2]);
+    iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24) //把相差的毫秒数转换为天数
+    return iDays
+}
+
+function sign2() {
+    //判断用户已登录
+    if (obj != "") {
+        document.getElementById("reg").style.display = "none";
+        document.getElementById("jmm").style.display = "block";
+        document.getElementById("ds").innerHTML = egg;
+        document.getElementById("ddr").innerHTML = lxts;
+        //判断蛋的个数
+        if (ts2 != 0) {
+            if (d == 1) {
+                document.getElementById("d1").style.display = "block";
+            } else if (d == 2) {
+                document.getElementById("d1").style.display = "block";
+                document.getElementById("d2").style.display = "block";
+            }
+            document.getElementById("qd").style.animationName = 'dd';
+        }
+        //判断鸡的大小
+        if (zt == 'min') {
+            document.getElementById("jmm").style.backgroundImage = "url('./images/xjj.gif')";
+        } else if (zt == 'max') {
+            document.getElementById("jmm").style.backgroundImage = "url('./images/jmm.png')";
+        }
+        //判断背景黑夜
+        if (time >= 20 || time <= 6) {
+            document.getElementById("yw").style.display = "block";
+            if (zt == 'max') {
+                document.getElementById("jmm").style.backgroundImage = "url('./images/jmm2.png')";
+            }
+        }
+        if (zt == 'max') {
+            document.getElementById('ww').innerHTML = '收取';
+        }
+    }
+
+}
+window.onload = function() {
+    sign2()
+    if (ts2 == 0) {
+        if (zt == "max") {
+            document.getElementById('ww').innerHTML = '已收取';
+        } else if (zt == "min") {
+            document.getElementById('ww').innerHTML = '成长中';
+        } else if (zt != "min" && zt != "max") {
+            document.getElementById('ww').innerHTML = '孵化中';
+        }
+    }
+    var zbc = "2020-12-12"
+    var bee = "2020-12-11"
+    var bhdex = "2021-5-7"
+    var vgc = "2021-6-17"
+    var bi = "2021-6-24"
+    var gmn = "2021-8-13"
+    var yb = "2021-8-9"
+    var bg = "2021-9-17"
+    s = cur(); // 获取当前时间
+    document.getElementById("zbc").innerHTML = DateDiff(zbc, s); //计算天数差的函数
+    document.getElementById("bee").innerHTML = DateDiff(bee, s);
+    document.getElementById("bhdex").innerHTML = DateDiff(bhdex, s);
+    document.getElementById("vgc").innerHTML = DateDiff(vgc, s);
+    document.getElementById("bi").innerHTML = DateDiff(bi, s);
+    document.getElementById("gmn").innerHTML = DateDiff(gmn, s);
+    document.getElementById("yb").innerHTML = DateDiff(yb, s);
+    document.getElementById("bg").innerHTML = DateDiff(bg, s);
+}
+
+function register() {
+    var a = document.getElementById("mz").value;
+    var b = document.getElementById("mm").value;
+    if (a != "" && b != "") {
+        let params = {
+            username: document.getElementById("mz").value,
+            password: document.getElementById("mm").value,
+            // phone: document.getElementById("sjh").value,
+            // qqh: document.getElementById("qqh").value,
+            egg: 0,
+            lxts: 0,
+            sj: cur(),
+        }
+
+        Bmob.User.register(params).then(res => {
+            alert("注册成功,点击登录");
+        }).catch(err => {
+            alert("该账户已注册！");
+        });
+    } else {
+        alert("不能有空哦")
+    }
+}
+
+function sign() {
+    var a = document.getElementById("mz").value;
+    var b = document.getElementById("mm").value;
+    if (a != "" && b != "") {
+        Bmob.User.login(document.getElementById("mz").value,
+            document.getElementById("mm").value).then(res => {
+            alert("你好：" + a);
+            location.reload();
+        }).catch(err => {
+            alert("名字或密码错误！");
+        });
+    } else {
+        alert("不能有空哦")
+    }
+}
+Bmob.User.updateStorage(obj) //更新数据
